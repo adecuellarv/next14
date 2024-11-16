9d7f5bfd8c11e51e1def823001a74e0873927d46
Ade Cuellar <76180286+adecuellarv@users.noreply.github.com>
2024-11-15 23:17:26 -0600

    no found page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 
-
 export default function Home() {
-  return (
-    <div className="">
-      <main className="">
-        <h1>Home</h1>
-      </main>
-     
-    </div>
-  );
-}
+    return (
+      <div className="">
+        <main className="">
+          <h1>Home</h1>
+        </main>
+       
+      </div>
+    );
+  }
+  
--- src/components/shared/Footer/Footer.tsx
+++ src/components/shared/Footer/Footer.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 const Footer = () => {
     return (
         <footer className="bg-white rounded-lg shadow dark:bg-gray-900 m-4">
@@ -6,7 +5,7 @@ const Footer = () => {
                 <div className="sm:flex sm:items-center sm:justify-between">
                     <a href="https://flowbite.com/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
                         <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
-                        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Ade Cuellar</span>
+                        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Footer</span>
                     </a>
                 </div>
                 <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
